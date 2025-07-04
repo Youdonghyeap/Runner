1ecb9aeb1eb3ed01a423e9e023d663d42c0129de
DESKTOP-B3EL7F1\202_27 <backjung0823@naver.com>
2025-07-04 11:03:37 +0900

    애니메이션, 사운드 추가

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public AudioClip deathClip;
+    public float jumpForce = 700f;
+    public int jumpcount = 0;
+    public bool isGrounded = false;
+    private bool isDead = false;
+
+    private Rigidbody2D playerRigidbody;
+    private Animator animator;
+    private AudioSource playerAudio;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDead)
+        {
+            return;
+        }
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame && jumpcount < 2)
+        {
+            jumpcount++;
+            playerRigidbody.linearVelocity = Vector2.zero; // Reset velocity before applying jump force
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            playerAudio.Play();
+        }
+        else if (Mouse.current != null && Mouse.current.leftButton.wasReleasedThisFrame && playerRigidbody.linearVelocity.y > 0)
+        {
+            playerRigidbody.linearVelocity = playerRigidbody.linearVelocity * 0.5f; // Reduce upward velocity when mouse button is released
+        }
+        animator.SetBool("Grounded", isGrounded);
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Die");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        isDead = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpcount = 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
