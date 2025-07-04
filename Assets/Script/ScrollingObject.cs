16b53c8f195a023998671db359e703925c551c91
DESKTOP-B3EL7F1\202_27 <backjung0823@naver.com>
2025-07-04 11:32:50 +0900

    사물 움직이기, backgroundLoop 적용

---
+++ Assets/Script/ScrollingObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
